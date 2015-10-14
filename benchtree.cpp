a126d2e64b5a18d4df21f3647cbefac278ed7eae
paulszh <paulszh@pauslzhtekiMacBook-Pro.local>
2015-10-14 12:50:10 -0700

    Orignial version

---
+++ benchtree.cpp
@@ -0,0 +1,12 @@
+#include "RST.hpp"
+#include "countint.hpp"
+
+using namespace std;
+
+
+
+int main(int argc, char** argv) {
+    cout << "# Benchmarking average number of comparisons for successful find" << endl;
+
+    return 0;
+}
