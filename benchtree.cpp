52966b71aaf605217dd33f84989544c9a0d7a394
paulszh <paulszh@pauslzhtekiMacBook-Pro.local>
2015-10-20 10:20:29 -0700

    bench tree revised

--- benchtree.cpp
+++ benchtree.cpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <string>
 #include <math.h>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -16,7 +17,7 @@ int main(int argc, char* argv[]) {
 
     int range = atoi(argv[3]);
 
-    if(range < 1){
+    if(range < 1){  //FIXME must also check if this number is the power of 2
         cout << "The range is invalid, please check" << endl;
         return 0;
     }
@@ -31,8 +32,7 @@ int main(int argc, char* argv[]) {
     string current = argv[1];
     string datatype = argv[2];
 
-    double stdev,difference,acpr,cpr,total,totalstd,avgstd;
-
+    double stdev,acpr,cpr,total,totalstd,avgstd;
     int expo, val;   // the expo of the
 
     cout << "# Benchmarking average number of comparisons for successful find" << endl;
@@ -41,68 +41,56 @@ int main(int argc, char* argv[]) {
     cout << "# N is the power of 2, minus 1, from 1 to " << range << endl;
     cout << "# Averaging "<< numRun << " runs for each N" <<endl;
     cout << "#" << endl;
-    cout << "# N 	 avgcomps 	 stdev" << endl;
-
-
-    if(current.compare(rststr) == 0){
-
-        RST<countint> r = RST<countint>();   //ceate a rst which contains countint
-        std::vector<countint> v;             //
-        countint::clearcount();
-        v.clear();
-
-        if(datatype.compare(shufflestr) == 0){
-            random_shuffle ( v.begin(), v.end());
-        }
-
-        for(expo = 1; exp2(expo)<= range; expo++){
-            val = exp2(expo)-1;
-            for(int i=0; i<val; ++i) {    //Add 0 to N to the vector
-                v.push_back(i);
-            }
-
-            std::vector<countint>::iterator vit = v.begin();
-            std::vector<countint>::iterator ven = v.end();
-
-            for(int i = 0; i < numRun; i++){  //the outer loop runs for numRun times
-
-                for(vit = v.begin(); vit != ven; ++vit){
-                    r.insert(*vit);
-                }
-
-                countint::clearcount();
-                for(vit = v.begin(); vit != ven; ++vit) {
-                    r.find(*vit);
-                }
-
-                cpr = countint::getcount()/(double)val;
-                total = total + cpr;
-                totalstd += cpr * cpr;
-            }
-            acpr = total/(double)numRun;  // average comparison per run
-            avgstd = totalstd/(double)numRun;
-            stdev = sqrt(abs(avgstd-acpr * acpr));
-            cout << val << "   "<< acpr <<"   " <<stdev <<endl;
-            total = 0;
-            totalstd = 0;
-        }
-    }
+    cout << "# N" <<setw(15) <<"avgcomps"<< setw(12) <<"stdev" << endl;
 
     if(current.compare(bststr) == 0){
 
-        BST<countint> r = BST<countint>();   //ceate a rst which contains countint
         std::vector<countint> v;             //
-        countint::clearcount();
-        v.clear();
-
-        if(datatype.compare(shufflestr) == 0){
-            random_shuffle ( v.begin(), v.end());
-        }
-
         for(expo = 1; exp2(expo)<= range; expo++){
             val = exp2(expo)-1;
+            v.clear();
             for(int i=0; i<val; ++i) {    //Add 0 to N to the vector
                 v.push_back(i);
+            }
+            if(datatype.compare(shufflestr) == 0){
+                random_shuffle ( v.begin(), v.end());
+            }
+            std::vector<countint>::iterator vit = v.begin();
+            std::vector<countint>::iterator ven = v.end();
+            for(int i = 0; i < numRun; i++){  //the outer loop runs for numRun times
+
+                BST<countint> b = BST<countint>();   //ceate a bst which contains countint
+                for(vit = v.begin(); vit != ven; ++vit){
+                    b.insert(*vit);
+                }
+                countint::clearcount();
+                for(vit = v.begin(); vit != ven; ++vit) {
+                    b.find(*vit);
+                }
+                cpr = countint::getcount()/(double)val;
+                total = total + cpr;
+                totalstd += cpr * cpr;
+            }
+            acpr = total/(double)numRun;  // average comparison per run
+            avgstd = totalstd/(double)numRun;
+            stdev = sqrt(abs(avgstd-acpr * acpr));
+            cout << setw(7) << val << setw(10) << acpr << setw(15)<<stdev <<endl;
+            total = 0;
+            totalstd = 0;
+        }
+    }
+    
+    if(current.compare(rststr) == 0){
+
+        std::vector<countint> v;
+        for(expo = 1; exp2(expo)<= range; expo++){
+            val = exp2(expo)-1;
+            v.clear();
+            for(int i=0; i<val; ++i) {    //Add 0 to N to the vector
+                v.push_back(i);
+            }
+            if(datatype.compare(shufflestr) == 0){
+                random_shuffle ( v.begin(), v.end());
             }
 
             std::vector<countint>::iterator vit = v.begin();
@@ -110,6 +98,7 @@ int main(int argc, char* argv[]) {
 
             for(int i = 0; i < numRun; i++){  //the outer loop runs for numRun times
 
+                RST<countint> r = RST<countint>();   //ceate a rst which contains countint
                 for(vit = v.begin(); vit != ven; ++vit){
                     r.insert(*vit);
                 }
@@ -126,43 +115,41 @@ int main(int argc, char* argv[]) {
             acpr = total/(double)numRun;  // average comparison per run
             avgstd = totalstd/(double)numRun;
             stdev = sqrt(abs(avgstd-acpr * acpr));
-            cout << val << "   "<< acpr <<"   " <<stdev <<endl;
+            cout << setw(7) << val << setw(10) << acpr << setw(15)<<stdev <<endl;
             total = 0;
             totalstd = 0;
         }
     }
 
+
+
     if(current.compare(setstr) == 0){
 
-        set<countint> r = set<countint>();   //ceate a rst which contains countint
         std::vector<countint> v;             //
         countint::clearcount();
-        v.clear();
-
-        if(datatype.compare(shufflestr) == 0){
-            random_shuffle ( v.begin(), v.end());
-        }
 
         for(expo = 1; exp2(expo)<= range; expo++){
             val = exp2(expo)-1;
+            v.clear();
             for(int i=0; i<val; ++i) {    //Add 0 to N to the vector
                 v.push_back(i);
+            }
+            if(datatype.compare(shufflestr) == 0){
+                random_shuffle ( v.begin(), v.end());
             }
 
             std::vector<countint>::iterator vit = v.begin();
             std::vector<countint>::iterator ven = v.end();
 
             for(int i = 0; i < numRun; i++){  //the outer loop runs for numRun times
-
+                set<countint> s = set<countint>();   //ceate a rst which contains countint
                 for(vit = v.begin(); vit != ven; ++vit){
-                    r.insert(*vit);
+                    s.insert(*vit);
                 }
-
                 countint::clearcount();
                 for(vit = v.begin(); vit != ven; ++vit) {
-                    r.find(*vit);
+                    s.find(*vit);
                 }
-
                 cpr = countint::getcount()/(double)val;
                 total = total + cpr;
                 totalstd += cpr * cpr;
@@ -170,7 +157,7 @@ int main(int argc, char* argv[]) {
             acpr = total/(double)numRun;  // average comparison per run
             avgstd = totalstd/(double)numRun;
             stdev = sqrt(abs(avgstd-acpr * acpr));
-            cout << val << "   "<< acpr <<"   " <<stdev <<endl;
+            cout << setw(7) << val << setw(10) << acpr << setw(15)<<stdev <<endl;
             total = 0;
             totalstd = 0;
         }
